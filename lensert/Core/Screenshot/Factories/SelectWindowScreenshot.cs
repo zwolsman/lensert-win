8f01d23e81a6ae4fa92078fa408da682c6a149ea
Joël Luijmes <joelluijmes@gmail.com>
2017-02-11 21:04:42 +0100

    fix select window

--- lensert/Core/Screenshot/Factories/SelectWindowScreenshot.cs
+++ lensert/Core/Screenshot/Factories/SelectWindowScreenshot.cs
@@ -17,9 +17,25 @@ namespace Lensert.Core.Screenshot.Factories
 
         private void SelectionForm_MouseMove(object sender, MouseEventArgs e)
         {
-            var selectedRectangle = _rectangles.FirstOrDefault(r => r.Contains(e.Location));
+            var x = e.X;
+            var y = e.Y;
+
+            // take account for monitors on the left/top
+            if (Native.UnscaledBounds.X < 0)
+                x += Native.UnscaledBounds.X;
+            if (Native.UnscaledBounds.Y < 0)
+                y += Native.UnscaledBounds.Y;
+
+            var location = new Point(x, y);
+            var selectedRectangle = _rectangles.FirstOrDefault(r => r.Contains(location));
             if (selectedRectangle == default(Rectangle))
                 return;
+
+            // at painting, the left/top monitor is the 0,0 so again take account for that
+            if (Native.UnscaledBounds.X < 0)
+                selectedRectangle.X -= Native.UnscaledBounds.X;
+            if (Native.UnscaledBounds.Y < 0)
+                selectedRectangle.Y -= Native.UnscaledBounds.Y;
 
             SelectionForm.SelectedArea = selectedRectangle;
         }
