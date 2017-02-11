10446c08127dc6f9cb6803aaaac21a42bfeb18e4
Joël Luijmes <joelluijmes@gmail.com>
2017-02-11 17:48:37 +0100

    updated assembly info

--- lensert-updater/Properties/AssemblyInfo.cs
+++ lensert-updater/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LensertInstaller")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("lensert-updater")]
+[assembly: AssemblyDescription("Updater for Lensert.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LensertInstaller")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("lensert-updater")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
